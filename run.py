32c9f75da13cbc9526587edb979806c95720ddb2
zNhaan <145633040+zNhaan@users.noreply.github.com>
2024-09-19 09:46:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-import os
+import os , time
 
 if __name__ == "__main__":
   with open("config.txt" , "r") as file:
     config = file.read()
   while True:
     os.system(f"java -jar mc-bots-1.2.11.jar -s {config} -r -ar 1")
+    sleep(1)
