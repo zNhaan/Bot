c65d367eb5fa0d7a73524875d66f633b72c50be6
zNhaan <145633040+zNhaan@users.noreply.github.com>
2024-09-19 09:51:31 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import os , time
+import os
+import time
 
 if __name__ == "__main__":
-  with open("config.txt" , "r") as file:
-    config = file.read()
-  os = f"java -jar mc-bots-1.2.11.jar -s {config} -r -ar 1"
-  while True:
-    os.system(os)
-    sleep(1)
+    with open("config.txt", "r") as file:
+        config = file.read().strip()  # Thêm .strip() để loại bỏ khoảng trắng thừa
+    command = f"java -jar mc-bots-1.2.11.jar -s {config} -r -ar 1"
+    while True:
+        os.system(command)
+        time.sleep(1)
