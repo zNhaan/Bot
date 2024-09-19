b7b1bdce4b8c1afd14444eb76eb58e22181bb981
zNhaan <145633040+zNhaan@users.noreply.github.com>
2024-09-19 10:40:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 config=$(cat config.txt | xargs)  # xargs dùng để loại bỏ khoảng trắng thừa
 
 # Câu lệnh java cần chạy
-command="java -jar mc-bots-1.2.11.jar -s ${config} -r -ar 1"
+command="java -jar mc-bots-1.2.11.jar -s ${config} -r -ar 1 -j Cặc"
 
 # Vòng lặp vô hạn để thực hiện câu lệnh liên tục
 while true; do
